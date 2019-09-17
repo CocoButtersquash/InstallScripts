af25ef5068b8f242cc06436cfc8bb4e8c486b71f
jack.wilson <jack.wilson@qa.com>
2019-09-17 13:31:33 +0100

    initial commit with python 3.6 for Redhat

---
+++ README.md
@@ -0,0 +1,5 @@
+##Install Python 3
+
+This script is designed to install python 3.6 on redhat 7.
+
+
---
+++ python36.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+pushd /tmp
+curl https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -o epel-release-latest-7.noarch.rpm
+yum install -y epel-release-latest-7.noarch.rpm
+yum update -y
+yum install -y yum-utils
+yum groupinstall -y development
+yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+yum install -y python36u python36u-pip
+yum install -y screen
+popd
+
